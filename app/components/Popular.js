d80be816ba47aa053c0cfa1fc443f49c4e489ee0
Chris Vita <chris.vita@gmail.com>
2017-05-01 16:39:08 -0700

    "Initial commit"

---
+++ app/components/Popular.js
@@ -0,0 +1,37 @@
+var React = require('react');
+
+class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        };
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(lang) {
+        this.setState(function () {
+            return {
+                selectedLanguage: lang
+            }
+        });
+    }
+    render() {
+        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+        return (
+            <ul className='languages'>
+                {languages.map(function (language) {
+                    return (
+                        <li
+                            style={language === this.state.selectedLanguage ? {color: '#d0021b'} : null}
+                            key={language}
+                            onClick={this.updateLanguage.bind(null, language)}>
+                            {language}
+                        </li>
+                    );
+                }, this)}
+            </ul>
+        );
+    }
+}
+
+module.exports = Popular; 
